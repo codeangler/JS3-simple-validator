ffbc3e40996c08cb0eb86f143f2774a17a8eebee
Casey Burnett <burnett.casey@gmail.com>
2016-05-19 12:18:37 -0600

    script update with phone validation

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert('setup');
+// request phone number from user
+var phoneNum = prompt('Please enter your ###-###-####');
+
+// Test submission for 
+alert (phoneNum.charAt(3) === "-" && phoneNum.charAt(7) === "-");
